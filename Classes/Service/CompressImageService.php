fd326106810cda98246138079af980b0afe8dff5
Sven Friese <sven.friese@interlutions.de>
2017-04-28 15:30:10 +0200

    [TASK] inject objectManager, add composer meta info

--- Classes/Service/CompressImageService.php
+++ Classes/Service/CompressImageService.php
@@ -3,31 +3,21 @@ namespace Schmitzal\Tinyimg\Service;
 
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\Folder;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
-use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
 use TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility;
 
 require_once(__DIR__ . '/../../vendor/autoload.php');
 
 /**
- * Class CompressImageSerivce
+ * Class CompressImageService
  * @package Schmitzal\Tinyimg\Service
  */
 class CompressImageService
 {
     /**
-     * @var ObjectManager
+     * @var \TYPO3\CMS\Extbase\Object\ObjectManager
+     * @inject
      */
     protected $objectManager;
-
-    /**
-     * CompressImageService constructor.
-     * @param ObjectManagerInterface $objectManager
-     */
-    public function __construct(ObjectManagerInterface $objectManager)
-    {
-        $this->objectManager = $objectManager;
-    }
 
     /**
      * @param File $file
@@ -38,8 +28,9 @@ class CompressImageService
         \Tinify\setKey($this->getApiKey());
 
         if (in_array($file->getExtension(), ['png', 'jpg'], true)) {
-            $source = \Tinify\fromFile(PATH_site . $file->getPublicUrl());
-            $source->toFile(PATH_site . $file->getPublicUrl());
+            $publicUrl = PATH_site . $file->getPublicUrl();
+            $source = \Tinify\fromFile($publicUrl);
+            $source->toFile($publicUrl);
         }
     }
 
