c396b525accda2a2215bb06ea33f94b037ff3bf6
Alex <a.schmitz1993@gmail.com>
2017-10-09 17:12:55 +0200

    Feature/compression bar (#17)
    * [FEATURE] Add a compression bar after file has been uploaded & show a message when compression is done with the percentage saved
    
    Resolves #11

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,5 +1,9 @@
 module.tx_tinyimg.settings.debug = 0
 
+module.tx_filelist.view {
+    templateRootPaths.0 = EXT:tinyimg/Resources/Private/Templates/
+}
+
 [applicationContext = Development]
 module.tx_tinyimg.settings.debug = 1
 [global]
