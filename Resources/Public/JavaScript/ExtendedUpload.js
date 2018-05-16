3f34cf5f739ae9f9821aa58bfe79ed9a93ead509
Alex <a.schmitz1993@gmail.com>
2018-05-16 17:00:42 +0200

    Feature/compression task (#21)
    [FEATURE] Add an extbase controller which loops through all file storage and compresses all images recursively in it
    
    Resolves #9

--- Resources/Public/JavaScript/ExtendedUpload.js
+++ Resources/Public/JavaScript/ExtendedUpload.js
@@ -4,14 +4,17 @@ define([
 ], function ($, Notification) {
     $(document).ready(function () {
         $('.t3js-drag-uploader-trigger').on('updateProgress', function (event, me, percentage) {
-            if (percentage == '100%') {
+            if (percentage === '100%' && me.file.type.match(/image\/(jpg|jpeg|png)$/i)) {
                 me.$progressPercentage.text('');
                 me.updateMessage($('.tinyimg-compressing').text());
                 me.$row.addClass('compressing');
             }
         }).on('uploadSuccess', function (event, me, data) {
-            var percentageSaved = Math.round(100 - ((data.upload[0].size / me.file.size) * 100));
-            Notification.info('Compression', 'Tinyimg saved you about ' + percentageSaved + '% of your file size!', 10);
+            if (me.file.type.match(/image\/(jpg|jpeg|png)$/i)) {
+                var percentageSaved = Math.round(100 - ((data.upload[0].size / me.file.size) * 100));
+                me.$row.removeClass('compressing');
+                Notification.info('Compression', 'Tinyimg saved you about ' + percentageSaved + '% of your file size!', 10);
+            }
         });
     });
 });
