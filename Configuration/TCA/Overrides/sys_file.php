1b54a65ebbd0226a6cd3d9709e353c8196cd1503
Benni Mack <benjamin.mack@gmail.com>
2021-12-14 10:13:25 +0100

    [BUGFIX] Fix PHP 8.0 issue

--- Configuration/TCA/Overrides/sys_file.php
+++ Configuration/TCA/Overrides/sys_file.php
@@ -22,4 +22,9 @@ $sysFileColumns = [
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file', $sysFileColumns);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('sys_file', 'compress_error', '', '');
 
-$GLOBALS['TCA']['sys_file']['interface']['showRecordFieldList'] .= ',compressed,compress_error';
+
+if (isset($GLOBALS['TCA']['sys_file']['interface']['showRecordFieldList'])) {
+    $GLOBALS['TCA']['sys_file']['interface']['showRecordFieldList'] .= ',compressed,compress_error';
+} else {
+    $GLOBALS['TCA']['sys_file']['interface']['showRecordFieldList'] = 'compressed,compress_error';
+}
